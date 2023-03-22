ebe03fa397c48abde0ea833241e9c26e4e9b9653
AndrewSichkarenko <127544713+AndrewSichkarenko@users.noreply.github.com>
2023-03-22 19:41:52 +0200

    Update mainWin.py

--- mainWin.py
+++ mainWin.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget , QPushButton,QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QRadioButton,QButtonGroup,QLineEdit, QGroupBox, QListWidget
-from file import *
+from PyQt5.QtGui import QFont
+from instr import *
 from second_win import *
 
 class MainWin(QWidget):
@@ -19,8 +20,8 @@ class MainWin(QWidget):
         self.instruction = QLabel(txt_instruction)
         self.btn_next = QPushButton(txt_next, self)
         self.layout_line = QVBoxLayout()
-        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
-        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignCenter)
+        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignCenter)
         self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)
         self.setLayout(self.layout_line)
     def next_click(self):
