d62e5ea6c38a9e650a4f1e44b56df4c76b2b6be7
AndrewSichkarenko <127544713+AndrewSichkarenko@users.noreply.github.com>
2023-03-23 18:46:40 +0200

    Update mainWin.py

--- mainWin.py
+++ mainWin.py
@@ -2,11 +2,10 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget , QPushButton,QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QRadioButton,QButtonGroup,QLineEdit, QGroupBox, QListWidget
 from PyQt5.QtGui import QFont
 from instr import *
-from second_win import *
-
-class MainWin(QWidget):
-    def __init__(self):
-        super().__init__()
+from final_win import *
+class TestWin(QWidget):
+    def init(self):
+        super().init()
         self.set_appear()
         self.initUI()
         self.connects()
@@ -16,19 +15,46 @@ class MainWin(QWidget):
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
     def initUI(self): 
-        self.hello_text = QLabel(txt_hello)
-        self.instruction = QLabel(txt_instruction)
-        self.btn_next = QPushButton(txt_next, self)
-        self.layout_line = QVBoxLayout()
-        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignCenter)
-        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignCenter)
-        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)
-        self.setLayout(self.layout_line)
+        self.hor_line = QHBoxLayout()
+        self.v_line = QVBoxLayout()
+        self.v2_line = QVBoxLayout()
+
+        self.hintname = QLabel(txt_hintname)
+        self.hintage = QLabel(txt_hintage)
+        self.test1 = QLabel(txt_test1)
+        self.test2 = QLabel(txt_test2)
+        self.test3 = QLabel(txt_test3)
+        self.timer = QLabel('00:00:00')
+        self.starttest1 = QPushButton(txt_starttest1)
+        self.starttest2 = QPushButton(txt_starttest2)
+        self.starttest3 = QPushButton(txt_starttest3)
+        self.sendresults = QPushButton(txt_sendresults)
+        self.name = QLineEdit('П.І.Б.')
+        self.age = QLineEdit('0')
+        self.test1_result = QLineEdit('0')
+        self.test2_result = QLineEdit('0')
+        self.test3_result = QLineEdit('0')
+
+        self.v_line.addWidget(self.hintname, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.name, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.hintage, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.age, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.test1, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.starttest1, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.test1_result, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.test2, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.starttest2, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.test2_result, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.test3, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.starttest3, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.test3_result, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.sendresults, alignment = Qt.AlignCenter)
+        self.v2_line.addWidget(self.timer, alignment = Qt.AlignRight)
+        self.hor_line.addLayout(self.v_line)
+        self.hor_line.addLayout(self.v2_line)
+        self.setLayout(self.hor_line)
     def next_click(self):
-        self.tw= TestWin()
+        self.fw= FinalWin()
         self.hide()
     def connects(self): 
-        self.btn_next.clicked.connect(self.next_click)
-app = QApplication([])
-mw = MainWin()
-app.exec_()
+        self.sendresults.clicked.connect(self.next_click)
